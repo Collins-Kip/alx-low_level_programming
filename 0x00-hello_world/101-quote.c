ecd0a4c336be486ba5659c90d907208fd583f61d
Collins-Kip <collinsdaroderick@yahoo.com>
2023-02-20 14:22:00 -0800

    All done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main -Entry point
+ *
+ * Description: 'program that prints exactly as given'
+ *
+ * Return: Always 1 (Successful)
+ */
+
+int main(void) {
+fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return 1;
+}
