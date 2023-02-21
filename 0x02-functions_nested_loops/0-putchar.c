b2d94896b3ab6a1ba908c88d214052a839c1b089
Collins-Kip <collinsdaroderick@yahoo.com>
2023-02-21 13:50:22 -0800

    My first one today

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Maing - Entry point
+ *
+ * Description: Program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
